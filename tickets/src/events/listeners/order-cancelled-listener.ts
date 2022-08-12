1032ed57c025facb3ac0c69480ebe77e3b69ab99
Islam Ahmed <islam.abdalkarim@outlook.com>
2022-08-12 12:08:19 +0200

    Add order created and cancelled listeners to ticketing service

--- tickets/src/events/listeners/order-cancelled-listener.ts
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -1,19 +1,18 @@
-import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from "@islamahmed93/common";
+import { Listener, OrderCancelledEvent, Subjects } from "@islamahmed93/common";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models";
 import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
-export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
-    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+    subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
     queueGroupName = "tickets-service";
-    async onMessage(data: OrderCreatedEvent['data'], msg: Message): Promise<void> {
+    async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
         const ticket = await Ticket.findById(data.ticket.id);
         if (!ticket) {
-            throw new Error("Ticket not found");
+            throw new Error('Ticket Not Found');
         }
-        ticket.set({orderId: data.id});
+        ticket.set({orderId: undefined});
         await ticket.save();
-
         new TicketUpdatedPublisher(this.client).publish({
             id: ticket.id,
             price: ticket.price,
@@ -22,8 +21,6 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
             userId: ticket.userId,
             orderId: ticket.orderId
         });
-
         msg.ack();
     }
-    
 }
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
@@ -19,14 +21,21 @@ const start = async () => {
     throw new Error("NATS_CLIENT_ID must be defined");
   }
   try {
-    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
-    natsWrapper.client.on('close', () => {
-        console.log('NATS connection closed');
-        process.exit();
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
     });
-    process.on('SIGINT', () => natsWrapper.client.close());
-    process.on('SIGTERM', () => natsWrapper.client.close());
-    
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCancelledListener(natsWrapper.client).listen();
+    new OrderCreatedListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected To MongoDB");
   } catch (error) {
